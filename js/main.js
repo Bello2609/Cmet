5390689a02d2dce85cd177e0340b795e60d5022f
bello hadi <bellohadi82@gmail.com>
2022-03-24 15:47:21 +0100

    responsiveness have been added

--- js/main.js
+++ js/main.js
@@ -1,31 +1,46 @@
 //this is the beginning of the validation process
-const username = document.getElementById("username").value;
-const firstname = document.getElementById("firstname").value;
-const lastname = document.getElementById("lastname").value;
+const username = document.getElementById("username");
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
 
 const email = document.getElementById("email");
 const address = document.getElementById("address");
 const telephone = document.getElementById("telephone");
 const postal = document.getElementById("postal");
-const description = document.getElementById("textarea").value;
+const description = document.getElementById("textarea");
 const form = document.getElementById("form");
 
+// this is for the hambirger animation
+
+
+const navSlide = ()=>{
+    const navLink = document.querySelector(".nav");
+    
+    const burger_container = document.querySelector(".burger_container");
+
+    burger_container.addEventListener("click", ()=>{
+       
+        navLink.classList.toggle("nav_active");
+    });
+    console.log("clicked");
+}
+navSlide();
 
 // i will not  be adding other input field validation because it has been done in the file
-const validation = (e) =>{
+// const validation = (e) =>{
    
-    const password = document.getElementById("password").value;
-    const conpassword = document.getElementById("conpassword").value;
-    const passwordValue = password.trim().toString();
-    const conPasswordValue = conpassword.trim().toString();
-    e.preventDefault();
-    if(conPasswordValue === passwordValue){
-         return alert("match");
-    }
-    else{
-        return alert("Enter yor details");
-    }
+//     const password = document.getElementById("password").value;
+//     const conpassword = document.getElementById("conpassword").value;
+//     const passwordValue = password.trim().toString();
+//     const conPasswordValue = conpassword.trim().toString();
+//     e.preventDefault();
+//     if(conPasswordValue === passwordValue){
+//          return alert("match");
+//     }
+//     else{
+//         return alert("Enter yor details");
+//     }
         
     
-}
-form.addEventListener("submit", validation);
+// }
+// form.addEventListener("submit", validation);
